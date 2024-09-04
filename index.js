a5303b418f751d7b2dd8f772d21d10717b9309cb
Gautham <72859049+Gasu1@users.noreply.github.com>
2024-09-04 13:35:59 +0530

    final chrome extension commit

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
-function saveLead() {
-    console.log("Button clicked from onclick attribute")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+
+if (leadsFromLocalStorage){
+    myLeads =leadsFromLocalStorage
+    render(myLeads)
 }
 
-let inputBtn = document.getElementById("input-btn")
 
-inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener")
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    });
+    
+})
+
+function render(leads){
+    let listItems= ""
+
+    for (let i=0; i < leads.length ; i++ ){
+        listItems += `
+            <li>
+                <a target='_blank' href="${leads[i]}">
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+}
+
+ulEl.innerHTML = listItems
+
+}
+
+deleteBtn.addEventListener("dblclick",function(){
+    myLeads = []
+    localStorage.clear
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click",function(){
+    myLeads.push(inputEl.value)
+    inputEl.value=""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+        
 })
 
